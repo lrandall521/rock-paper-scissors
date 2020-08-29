c16176f1dff5521df89bcb74a9e5500b6aa978cb
lrandall521 <lrandall521@gmail.com>
2020-08-29 03:41:14 -0400

    added more js functions and improved html. wprl on the restart function next time, along with making the js work with html. do css way later

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,9 @@
-function computerPLay() {
+let pscore = 0;
+let cscore = 0;
+let record = "";
+let rounds;
+
+function computerPlay() {
   let r = Math.random() * 3;
   if (r < 1) return "Rock";
   if (r < 2) return "Paper";
@@ -14,14 +19,50 @@ function playRound(playerSelection, computerSelection) {
     (player == rock && computer == scissors) ||
     (player == scissors && computer == paper) ||
     (player == paper && computer == rock)
-  )
+  ) {
     return `You win! ${playerSelection} beats ${computerSelection}.`;
+    pscore++;
+  }
   //make name customizable in the future
   else if (
     (computer == rock && player == scissors) ||
     (computer == scissors && player == paper) ||
     (computer == paper && player == rock)
-  )
+  ) {
     return `The computer wins! ${computerSelection} beats ${playerSelection}.`;
-  else return "uh oh";
+    cscore++;
+  } else return "uh oh";
 }
+
+function newGame() {
+  record +=
+    playRound(document.getElementById("input").value, computerPlay()) + "\n";
+  document.querySelector("input").textContent = "";
+
+  if (pscore == rounds / 2 + 0.5) {
+    document.querySelector(
+      "input"
+    ).textContent = `You win! You beat the computer ${
+      rounds / 2 + 0.5
+    } best out of ${rounds}`;
+    return true;
+  }
+  if (cscore == rounds / 2 + 0.5) {
+    document.querySelector(
+      "input"
+    ).textContent = `You lose! The computer beat you ${
+      rounds / 2 + 0.5
+    } best out of ${rounds}`;
+    return true;
+  } else return false;
+}
+
+function roundsChoice() {
+  if (document.getElementsByName("rounds").value == false) {
+    alert("Please select a number of rounds");
+    return null;
+  }
+  rounds = Number(document.getElementsByName("rounds").value);
+}
+
+function restart() {}
