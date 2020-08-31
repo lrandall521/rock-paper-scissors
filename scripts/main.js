c81d08315a25e7854766329da31370b4e2128945
lrandall521 <lrandall521@gmail.com>
2020-08-31 19:52:27 -0400

    finished basic website. things to add: make name custom, make form able to be submitted by hitting enter, maybe make UI better?

--- scripts/main.js
+++ scripts/main.js
@@ -2,69 +2,76 @@ let pscore = 0;
 let cscore = 0;
 let record = "";
 let rounds = 3;
+let rounds_array = document.getElementsByName("rounds");
 
 function computerPlay() {
   let r = Math.random() * 3;
-  if (r < 1) return "rock";
-  if (r < 2) return "paper";
-  if (r < 3) return "scissors"; //make case insensitive
+  if (r < 1) return "Rock";
+  if (r < 2) return "Paper";
+  if (r < 3) return "Scissors"; //make case insensitive
 }
 
 function playRound(playerSelection, computerSelection) {
-  const player = playerSelection;
-  const computer = computerSelection;
+  const player = playerSelection.toLowerCase();
+  const computer = computerSelection.toLowerCase();
 
-  if (player == computer) return "Again!!";
+  if (player == computer) return "Tie!!";
   else if (
-    (player == rock && computer == scissors) ||
-    (player == scissors && computer == paper) ||
-    (player == paper && computer == rock)
+    (player == "rock" && computer == "scissors") ||
+    (player == "scissors" && computer == "paper") ||
+    (player == "paper" && computer == "rock")
   ) {
-    return `You win! ${playerSelection} beats ${computerSelection}.`;
     pscore++;
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
   }
   //make name customizable in the future
   else if (
-    (computer == rock && player == scissors) ||
-    (computer == scissors && player == paper) ||
-    (computer == paper && player == rock)
+    (computer == "rock" && player == "scissors") ||
+    (computer == "scissors" && player == "paper") ||
+    (computer == "paper" && player == "rock")
   ) {
-    return `The computer wins! ${computerSelection} beats ${playerSelection}.`;
     cscore++;
-  } else return "uh oh";
+    return `The computer wins! ${computerSelection} beats ${playerSelection}.`;
+  } else {
+    alert("Please type in 'Rock', 'Paper', or 'Scissors'");
+    return "";
+  }
 }
 
 function newGame() {
-  record +=
-    playRound(document.getElementById("input-games").value, computerPlay()) +
-    "\n";
-  document.getElementById("input-games").textContent = "";
+  record =
+    record +
+    playRound(document.querySelector("#input-games").value, computerPlay()) +
+    " ";
+  document.querySelector("#input-games").value = "";
   document.querySelector("p").textContent = record;
 
   if (pscore == rounds / 2 + 0.5) {
     document.querySelector(
-      "input"
+      "#final-position"
     ).textContent = `You win! You beat the computer ${
       rounds / 2 + 0.5
     } best out of ${rounds}`;
-    break;
+    return true;
   }
   if (cscore == rounds / 2 + 0.5) {
     document.querySelector(
-      "input"
+      "#final-position"
     ).textContent = `You lose! The computer beat you ${
       rounds / 2 + 0.5
     } best out of ${rounds}`;
-    break;
-  }
+    return true;
+  } else return false;
 }
 
 function roundsChoice() {
-  if (document.getElementsByName("rounds").value == false) {
-    alert("Please select a number of rounds");
-    return null;
+  for (i = 0; i < rounds_array.length; i++) {
+    if (rounds_array[i].checked) {
+      rounds = Number(rounds_array[i].value);
+      return true;
+    }
   }
-  rounds = Number(document.getElementsByName("rounds").value);
+  alert("Please select a number of rounds");
 }
 
 function restart() {
@@ -73,8 +80,8 @@ function restart() {
   record = "";
   rounds = 3;
 
-  document.getElementsByClassName("rounds-button").checked = false;
-  document.getElements("input-games").textContent = "";
+  for (i = 0; i < rounds_array.length; i++) rounds_array[i].checked = false;
+  document.querySelector("#input-games").value = "";
   document.querySelector("p").textContent = "";
   document.querySelector("h3").textContent = "";
 }
